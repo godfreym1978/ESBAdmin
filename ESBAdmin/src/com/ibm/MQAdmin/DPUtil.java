27edc81b08df5f15f3bfd3dd95332d8d6216245f
Godfrey Menezes <godfreym@gmail.com>
2015-05-28 13:40:56 -0400

    Added new methods for DP Data fetch.

--- ESBAdmin/src/com/ibm/MQAdmin/DPUtil.java
+++ ESBAdmin/src/com/ibm/MQAdmin/DPUtil.java
@@ -26,6 +26,7 @@ import com.ibm.datapower.wamt.clientAPI.ManagedSet;
 import com.ibm.datapower.wamt.clientAPI.Manager;
 import com.ibm.datapower.wamt.clientAPI.ProgressContainer;
 import com.ibm.datapower.wamt.clientAPI.RuntimeService;
+import com.ibm.mq.constants.MQConstants;
 
 public class DPUtil {
 
@@ -71,9 +72,233 @@ public class DPUtil {
 		}
 	}
 
+	public static List<Map> getDeviceDtl(){
+
+		List<Map> deviceListDtl = new ArrayList<Map>();
+		Map iMap = new HashMap();
+
+		try{
+			// Get an instance of the manager. All subsequent calls to
+			// getInstance will return the same
+			// instance since the manager is a singleton.
+			Manager manager = Manager.getInstance(null);
+
+			// Go ahead and declare the progressContainer var, it will be used a
+			// few times.
+			ProgressContainer progressContainer = null;
+
+			// Create device object, note the use of a ProgressContainer since
+			// the amount of time needed
+			// to communicate with the device is indeterminate.
+			
+			//Manager manager = Manager.getInstance(null);
+			ManagedSet ms = manager.getManagedSet("mgSet");
+			Device devices[] = ms.getDeviceMembers();
+
+			for (int deviceCtr = 0; deviceCtr < devices.length; deviceCtr ++) {
+			
+				iMap = new HashMap();
+				
+				iMap.put("ActualFirmware", devices[deviceCtr].getActualFirmwareLevel());
+				iMap.put("CurrentAMPVersion", devices[deviceCtr].getCurrentAMPVersion());
+				iMap.put("GUIPort", devices[deviceCtr].getGUIPort());
+				iMap.put("HLMPort", devices[deviceCtr].getHLMPort());
+				iMap.put("HostName", devices[deviceCtr].getHostname());
+				iMap.put("QuiesceTimeout", devices[deviceCtr].getQuiesceTimeout());
+				iMap.put("SerialNumber", devices[deviceCtr].getSerialNumber());
+				iMap.put("SymbolicName", devices[deviceCtr].getSymbolicName());
+				iMap.put("GetUserID", devices[deviceCtr].getUserId());
+				iMap.put("IsDeviceReachable", devices[deviceCtr].isDeviceReachable());
+				iMap.put("IsPrimary", devices[deviceCtr].isPrimary());
+				iMap.put("IsSourceBackupSupported", devices[deviceCtr].isSecureBackupSupported());
+				iMap.put("DeviceType", devices[deviceCtr].getDeviceType());
+				iMap.put("ModelType", devices[deviceCtr].getModelType());
+				iMap.put("SourceFirmware", devices[deviceCtr].getSourceFirmwareVersion());
+				
+				deviceListDtl.add(iMap);
+				
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return deviceListDtl;
+	}
+	
+	public List<Map> getDomains(String dpHostName){
+		List<Map> deviceListDtl = new ArrayList<Map>();
+		Map iMap = new HashMap();
+
+		try{
+			// Get an instance of the manager. All subsequent calls to
+			// getInstance will return the same
+			// instance since the manager is a singleton.
+			Manager manager = Manager.getInstance(null);
+
+			// Go ahead and declare the progressContainer var, it will be used a
+			// few times.
+			ProgressContainer progressContainer = null;
+
+			// Create device object, note the use of a ProgressContainer since
+			// the amount of time needed
+			// to communicate with the device is indeterminate.
+			
+			//Manager manager = Manager.getInstance(null);
+			ManagedSet ms = manager.getManagedSet("mgSet");
+			Device devices[] = ms.getDeviceMembers();
+			
+			
+			for (int deviceCtr = 0; deviceCtr < devices.length; deviceCtr ++) {
+				if (devices[deviceCtr].getHostname().equalsIgnoreCase(dpHostName)){
+					StringCollection sc = devices[deviceCtr].getAllDomainNames();
+					//int domainCtr = 0;
+					for (int domainCtr = 0; domainCtr < sc.size(); domainCtr ++) {
+						iMap = new HashMap();
+						//System.out.println(devices[deviceCtr].getSymbolicName());
+						iMap.put("SymbolicName", devices[deviceCtr].getSymbolicName());
+						iMap.put("DPHostName", devices[deviceCtr].getHostname());
+						iMap.put("DomainName", sc.get(domainCtr));
+						deviceListDtl.add(iMap);
+					}
+				}
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return deviceListDtl;
+		
+	}
+
+	public List<Map> getDomainServices(String dpHostName, String domainName){
+		List<Map> deviceListDtl = new ArrayList<Map>();
+		Map iMap = new HashMap();
+
+		try {
+			// Get an instance of the manager. All subsequent calls to
+			// getInstance will return the same
+			// instance since the manager is a singleton.
+			Manager manager = Manager.getInstance(null);
+
+			// Go ahead and declare the progressContainer var, it will be used a
+			// few times.
+			ProgressContainer progressContainer = null;
+
+			// Create device object, note the use of a ProgressContainer since
+			// the amount of time needed
+			// to communicate with the device is indeterminate.
+			
+			//Manager manager = Manager.getInstance(null);
+			ManagedSet ms = manager.getManagedSet("mgSet");
+			Device devices[] = ms.getDeviceMembers();
+
+			for (int deviceCtr = 0; deviceCtr < devices.length; deviceCtr ++) {
+				if(devices[deviceCtr].getHostname().equals(dpHostName)){
+					RuntimeService rs[] = devices[deviceCtr].getManagedDomain(domainName).getServices();
+					for (int serviceCtr = 0; serviceCtr < rs.length; serviceCtr ++) {
+						iMap = new HashMap();
+
+						iMap.put("Domain", rs[serviceCtr].getDomain().getAbsoluteDisplayName());
+						iMap.put("ClassDisplayName", rs[serviceCtr].getClassDisplayName());
+						iMap.put("DomainName", rs[serviceCtr].getName());
+						iMap.put("DomainUserComments", rs[serviceCtr].getUserComment());
+						iMap.put("OpStatus", rs[serviceCtr].getOpStatus());
+						iMap.put("PrimaryKey", rs[serviceCtr].getPrimaryKey());
+						iMap.put("QuiesceStatus", rs[serviceCtr].getQuiesceStatus());
+						iMap.put("AdminStatus", rs[serviceCtr].getAdminStatus());
+						deviceListDtl.add(iMap);
+					}
+				}
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return deviceListDtl;
+	}
+
 	// public void getServices(String symbolicName,String hostName,String
 	// userID, String password, int hlmPort){
-	public List<Map> getServices() {
+	public List<Map> getServices(String dpHostName, String domainName, String svcName) {
+		List<Map> ServiceDtl = new ArrayList<Map>();
+
+		Map iMap = new HashMap();
+		try {
+			
+			// Get an instance of the manager. All subsequent calls to
+			// getInstance will return the same
+			// instance since the manager is a singleton.
+			Manager manager = Manager.getInstance(null);
+
+			// Go ahead and declare the progressContainer var, it will be used a
+			// few times.
+			ProgressContainer progressContainer = null;
+
+			// Create device object, note the use of a ProgressContainer since
+			// the amount of time needed
+			// to communicate with the device is indeterminate.
+			
+			//Manager manager = Manager.getInstance(null);
+			ManagedSet ms = manager.getManagedSet("mgSet");
+			Device devices[] = ms.getDeviceMembers();
+			
+			/*Work on this*/
+			List<String> fileListDtl = new ArrayList<String>();
+			List<String> configListDtl = new ArrayList<String>();
+
+			for (int deviceCtr = 0; deviceCtr < devices.length; deviceCtr ++) {
+				if(devices[deviceCtr].getHostname().equals(dpHostName)){
+					RuntimeService rs[] = devices[deviceCtr].getManagedDomain(domainName).getServices();
+					for (int serviceCtr = 0; serviceCtr < rs.length; serviceCtr ++) {
+						
+						if(rs[serviceCtr].getName().equalsIgnoreCase(svcName)){
+							
+							iMap = new HashMap();
+							ReferencedObjectCollection refObjColl = rs[serviceCtr]
+									.getReferencedObjectsAndFiles();
+
+							try {
+								StringCollection refFiles = refObjColl
+										.getReferencedFiles();
+								for (int refFileCtr = 0;refFileCtr < refFiles.size();refFileCtr++){
+									fileListDtl.add(refFiles.get(refFileCtr)
+											.toString());
+								}
+								iMap.put("FileList", fileListDtl);
+								ServiceDtl.add(iMap);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+							try {
+								com.ibm.datapower.wamt.amp.ConfigObject[] configObj = refObjColl
+										.getReferencedObjects();
+								for (int configObjCtr = 0;configObjCtr < configObj.length;configObjCtr++){
+									configListDtl.add(configObj[configObjCtr]
+											.getName());
+								}
+								iMap.put("configListDtl", configListDtl);
+								ServiceDtl.add(iMap);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+
+							iMap.put("AdminStatus", rs[serviceCtr].getAdminStatus());
+							ServiceDtl.add(iMap);
+						}
+						/*Work on this*/
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.gc();
+		return ServiceDtl;
+	}
+
+	// public void getServices(String symbolicName,String hostName,String
+	// userID, String password, int hlmPort){
+	public List<Map> getDPEnvironment() {
 		List<Map> deviceListDtl = new ArrayList<Map>();
 		List<Map> mpgListDtl = new ArrayList<Map>();
 		List<Map> wspListDtl = new ArrayList<Map>();
@@ -86,14 +311,10 @@ public class DPUtil {
 			Manager manager = Manager.getInstance(null);
 			ManagedSet ms = manager.getManagedSet("mgSet");
 			Device devices[] = ms.getDeviceMembers();
-
 			Map<String, Map<String, String>> deviceDtl = new HashMap<String, Map<String, String>>();
-
 			Map<String, String> domainDtl = new HashMap<String, String>();
-
 			List<String> fileListDtl = new ArrayList<String>();
 			List<String> configListDtl = new ArrayList<String>();
-
 			int deviceCtr = 0;
 			while (deviceCtr < devices.length) {
 				StringCollection sc = devices[deviceCtr].getAllDomainNames();
